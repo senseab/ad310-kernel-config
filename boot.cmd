c2eb5c9b7ec984232094dd4416abe29edc42ec8d
TonyChyi <tonychee1989@gmail.com>
2015-02-10 21:04:03 +0800

    fix

--- boot.cmd
+++ boot.cmd
@@ -1,4 +1,4 @@
-setenv rootfs /dev/mmcblk1p2
+setenv rootfs PARTUUID=5a2a87ed-02
 setenv bootargs earlyprintk console=ttyS0,115200 init=/bin/init root=${rootfs} rootwait panic=10 ${extra}
 setenv boot_normal ext4load mmc 0 \${fdt_addr_r} \${fdtfile}\;ext4load mmc 0 0x40008000 zImage\;bootz 0x40008000 - \${fdt_addr_r}
 run boot_normal
